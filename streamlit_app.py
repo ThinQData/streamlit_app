1d01dcd800ddd5c14844b853ee269b44661b6659
Seho <sehochoi@cnspartner.com>
2023-01-13 14:48:23 -0500

    test

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('Test')
+
+# Example 1
+
+st.write('Welcome to ThinQ Streamlit!')
+st.write('Below is the web parameters')
+
+st.write(st.experimental_get_query_params())
+
+# Example 2
+
+st.write(1234)
